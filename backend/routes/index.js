b9363bbb399eafca5980c1d34de1f3ba5a2a3fb0
abhijit-hota <abhihota025@gmail.com>
2022-09-04 00:49:10 +0530

    add: budget routes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import budgetRouter from './budget.js';
 import organizationRouter from './organization.js';
 import userRouter from './user.js';
 
@@ -6,6 +7,7 @@ const apiRouter = Router();
 
 apiRouter.use('/user', userRouter);
 apiRouter.use('/org', organizationRouter);
+apiRouter.use('/budget', budgetRouter);
 
 apiRouter.get('/', async (req, res) => {
 	res.send('This api was made by the monkeys of narmada');
