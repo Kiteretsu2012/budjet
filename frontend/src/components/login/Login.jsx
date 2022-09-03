eb77cf3185403989266d8257d42592c0f1a12aa2
Rohith <rohithsuresh207@gmail.com>
2022-09-03 13:36:30 +0530

    configure chakra

--- frontend/src/components/login/Login.jsx
+++ frontend/src/components/login/Login.jsx
@@ -1,9 +1,12 @@
-import { VStack } from '@chakra-ui/react';
+import { Button, Heading, VStack } from '@chakra-ui/react';
 
 const Login = () => {
 	return (
 		<div>
-			<VStack as="form"></VStack>
+			<VStack as="form" bg="white" border="2px solid black" w="40%" h="100px">
+				<Heading>Sign up</Heading>
+				<Button>Hello World</Button>
+			</VStack>
 		</div>
 	);
 };
