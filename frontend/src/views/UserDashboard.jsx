351bec255f3cc8c8f205ef73819bffdd7ce8a317
Rohith <rohithsuresh207@gmail.com>
2022-09-03 22:58:19 +0530

    To avoid merge conflicts

--- frontend/src/views/UserDashboard.jsx
+++ frontend/src/views/UserDashboard.jsx
@@ -1,29 +1,17 @@
-import { Box, Button, Grid, GridItem, useColorModeValue } from '@chakra-ui/react';
+import { Box, Grid, GridItem, useColorModeValue } from '@chakra-ui/react';
 import React from 'react';
 import { useState } from 'react';
 import Empty from '../components/user-dashboard/Empty';
-import CreateTeamModal from '../components/organization-dashboard/CreateTeamModal';
 
 import Header from '../components/user-dashboard/Header';
 
 function UserDashboard() {
 	const [userDetails, setUserDetails] = useState(JSON.parse(localStorage.getItem('userDetails')));
-	const [isOpen, setIsOpen] = useState(false);
 	return (
 		<Box minH="100vh" bg={useColorModeValue('gray.100', 'gray.900')}>
 			<Header />
 			<Grid templateColumns="repeat(6, 1fr)" gap={6}>
-				<GridItem colSpan={2} w="100%" h="100vh" bg="blue.500">
-					<Button
-						colorscheme="teal"
-						onClick={() => {
-							setIsOpen((prevState) => !prevState);
-						}}
-					>
-						Open
-					</Button>
-					<CreateTeamModal isOpen={isOpen} onClose={() => setIsOpen(false)} />
-				</GridItem>
+				<GridItem colSpan={2} w="100%" h="100vh" bg="blue.500" />
 				<GridItem colSpan={1} w="100%" h="100vh" bg="blue.500" />
 				<GridItem colSpan={3} w="100%" h="100vh" bg="#EDE3E9">
 					<Empty />
